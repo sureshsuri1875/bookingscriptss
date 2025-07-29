a1ce2660c121692e59ad3aac5c95131c3b9ca159
sureshsuri1875 <sureshsuri777246@gmail.com>
2025-07-29 12:00:51 +0530

    Create Auto.js

---
+++ Auto.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Fetch and Display Data1
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Fetch data from an API and display it on the webpage
+// @author       You
+// @match        https://onlinebooking.sand.telangana.gov.in/Order/NEWBOOKING.aspx?KLM=*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    console.log("Auto Select Vehicles... Loading AutoSelectVehiclesWithCounter.js.js");
+    const script = document.createElement("script");
+    script.src = "https://sureshsuri1875.github.io.js/bookingscripts/AutoSelectVehiclesWithCounter.js";
+    document.body.appendChild(script);
+})();
